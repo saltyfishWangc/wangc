bb84f190053e81f61f982ed3bd8be9056f38deda
saltyfishWangc <741627532@qq.com>
2022-09-05 20:48:10 +0800

    添加docker编排配置

--- wangc-spring-boot/src/main/java/com/wangc/es/repository/ESBookRepository.java
+++ wangc-spring-boot/src/main/java/com/wangc/es/repository/ESBookRepository.java
@@ -2,7 +2,6 @@ package com.wangc.es.repository;
 
 import com.wangc.es.entity.ESBook;
 import org.elasticsearch.search.SearchHits;
-import org.springframework.data.domain.Page;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
@@ -15,6 +14,13 @@ import java.util.List;
 @Repository("esBookRepository")
 public interface ESBookRepository extends ElasticsearchRepository<ESBook, String> {
 
+    /**
+     * 根据关键字查询title或author
+     *
+     * @param title
+     * @param author
+     * @return
+     */
     List<ESBook> findByTitleOrAuthor(String title, String author);
 
 //    @Highlight(fields = {
